39d2242bb8590052c4aa2816817cef3def222a55
hasibarrafiul <fahad.com66.fk@gmail.com>
2023-02-26 15:28:12 +0600

    email changed to username

--- server/index.js
+++ server/index.js
@@ -29,12 +29,12 @@ app.use(bodyParser.urlencoded({ extender: true }));
 
 app.get("/user/verify_login/", (req, res) => {
     //console.log("verify login")
-    const email = req.query.email;
+    const username = req.query.username;
     const password = req.query.password;
-    console.log("data  "+email+" "+password)
+    console.log("data  "+username+" "+password)
     //console.log(email, password)
-    const get_user = "select id,password from users where email = ?";
-    db.query(get_user, [email], (err, result) => {
+    const get_user = "select id,password from users where username = ?";
+    db.query(get_user, [username], (err, result) => {
         //console.log(result)
         if (result.length === 0) {
             console.log("No user found");
@@ -52,48 +52,48 @@ app.get("/user/verify_login/", (req, res) => {
 
 
 
-app.post("/user/verifyemail/", (req, res) => {
-    const code = req.body.code;
-    const email =  req.body.email;
-    // console.log(code);
-    // console.log(email);
-async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  //let testAccount = await nodemailer.createTestAccount();
+// app.post("/user/verifyemail/", (req, res) => {
+//     const code = req.body.code;
+//     const email =  req.body.email;
+//     // console.log(code);
+//     // console.log(email);
+// async function main() {
+//   // Generate test SMTP service account from ethereal.email
+//   // Only needed if you don't have a real mail account for testing
+//   //let testAccount = await nodemailer.createTestAccount();
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: "rashikbuksh71@gmail.com", // generated ethereal user
-      pass: "ljeceenkkayemrqy", // generated ethereal password
-    },
-  });
+//   // create reusable transporter object using the default SMTP transport
+//   let transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     host: 'smtp.gmail.com',
+//     port: 587,
+//     secure: false, // true for 465, false for other ports
+//     auth: {
+//       user: "rashikbuksh71@gmail.com", // generated ethereal user
+//       pass: "ljeceenkkayemrqy", // generated ethereal password
+//     },
+//   });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Port Project" <rashikbuksh71@gmail.com', // sender address
-    to: email, // list of receivers
-    subject: "Verification Code ✔", // Subject line
-    text: "Welcome to Port Project. Your verification code is: "+code+ " ;Enjoy", // plain text body
-    html: "<b>Welcome to Port Project. Your verification code is: "+code+ " ;Enjoy</b>", // html body
-  });
+//   // send mail with defined transport object
+//   let info = await transporter.sendMail({
+//     from: '"Port Project" <rashikbuksh71@gmail.com', // sender address
+//     to: email, // list of receivers
+//     subject: "Verification Code ✔", // Subject line
+//     text: "Welcome to Port Project. Your verification code is: "+code+ " ;Enjoy", // plain text body
+//     html: "<b>Welcome to Port Project. Your verification code is: "+code+ " ;Enjoy</b>", // html body
+//   });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+//   console.log("Message sent: %s", info.messageId);
+//   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
+//   // Preview only available when sending through an Ethereal account
+//   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+//   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+// }
 
-main().catch(console.error);
+// main().catch(console.error);
     
-});
+// });
 
 // app.get("/user/forgot_password/", (req, res) => {
 //     //console.log("verify login")
