ab0c72be041c5015039ac0cae1c946f331919039
Hasib Ar Rafiul Fahim <85202959+hasibarrafiul@users.noreply.github.com>
2023-03-05 15:39:24 +0600

    current status

--- server/index.js
+++ server/index.js
@@ -307,6 +307,84 @@ app.post("/management/deleterecord", (req, res) => {
     );
 });
 
+
+
+//Insert Current Status
+app.post("/management/currentstatus", (req, res) => {
+    console.log("submit in backend");
+    const LV_name = req.body.LV_name;
+    const date_from_charpotro = req.body.date_from_charpotro;
+    const commodity = req.body.commodity;
+    const LA = req.body.LA;
+    const dest_from = req.body.dest_from;
+    const dest_to = req.body.dest_to;
+    const current_location = req.body.current_location;
+    const remark = req.body.remark;
+    const create_current_status =
+        "INSERT INTO current_status (LV_name, date_from_charpotro, commodity, LA, dest_from, dest_to, current_location, remark) VALUES (?,?,?,?,?,?,?,?)";
+    db.query(
+        create_current_status,
+        [LV_name, date_from_charpotro, commodity, LA, dest_from, dest_to, current_location, remark],
+        (err, result) => {
+            if (err) console.log(err);
+            console.log(result)
+            res.send(result);
+        }
+    );
+});
+//Get record Entry
+app.get("/management/getcurrentstatus", (req, res) => {
+    const sqlSelect = "SELECT * from current_status";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    });
+});
+//Update Job Entry
+app.post("/management/updatecurrentstatus", (req, res) => {
+    console.log("update job info in backend");
+    const id = req.body.id;
+    const LV_name = req.body.LV_name;
+    const date_from_charpotro = req.body.date_from_charpotro;
+    const commodity = req.body.commodity;
+    const LA = req.body.LA;
+    const dest_from = req.body.dest_from;
+    const dest_to = req.body.dest_to;
+    const current_location = req.body.current_location;
+    const remark = req.body.remark;
+    //console.log(id);
+    const sqlUpdate =
+        "UPDATE current_status SET LV_name=?, date_from_charpotro=?, commodity=?, LA=?, dest_from=?, dest_to=?, current_location=?, remark=?  where id= ?";
+    db.query(
+        sqlUpdate,
+        [LV_name, date_from_charpotro, commodity, LA, dest_from, dest_to, current_location, remark, id],
+        (err, result) => {
+            if (err) console.log(err);
+            //console.log(result)
+            // res.send(result).json({
+            //     success: true,
+            // });
+        }
+    );
+});
+//Delete Record Entry
+app.post("/management/deletecurrentstatus", (req, res) => {
+    console.log("Delete status in backend");
+    const id = req.body.record_id;
+    const sqlDelete =
+        "DELETE from current_status where id= ?";
+    db.query(
+        sqlDelete,
+        [id],(err, result) => {
+            if (err) console.log(err);
+            //console.log(result)
+            if(!err){
+                res.send("success");
+            }
+            
+        }
+    );
+});
+
 app.listen(3001, () => {
     console.log("Running on port 3001");
 });
