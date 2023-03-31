7c03d0fc22dff40016ceabc28b3150f1bbb0d13a
Hasib Ar Rafiul Fahim <85202959+hasibarrafiul@users.noreply.github.com>
2023-03-31 19:29:49 +0600

    chq_approval

---
+++ server/apis/chq_approval.js
@@ -0,0 +1,125 @@
+function addChq_approval(req, res, db){
+    //console.log("submit in backend");
+    const order_number = req.body.order_number;
+    const job_number = req.body.job_number;
+    const date_from_charpotro = req.body.date_from_charpotro;
+    const cp_number_from_charpotro = req.body.cp_number_from_charpotro;
+    const LA_name = req.body.LA_name;
+    const LV_name = req.body.LV_name;
+    const MV_name = req.body.MV_name;
+    const dest_from = req.body.dest_from;
+    const dest_to = req.body.dest_to;
+    const capacity_ton = req.body.capacity_ton;
+    const rate = req.body.rate;
+    const sixty_percent_payment = req.body.sixty_percent_payment;
+    const forty_percent_payment = req.body.forty_percent_payment;
+    const damarage = req.body.damarage;
+    const second_trip = req.body.second_trip;
+    const third_trip = req.body.third_trip;
+    const direct_trip = req.body.direct_trip;
+    const create_chq =
+        "INSERT INTO chq_approval (order_number, job_number, date_from_charpotro, cp_number_from_charpotro, LA_name, LV_name, MV_name, dest_from, dest_to, capacity_ton, rate, sixty_percent_payment, forty_percent_payment, damarage, second_trip, third_trip, direct_trip) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    db.query(
+        create_chq,
+        [
+            order_number,
+            job_number,
+            date_from_charpotro,
+            cp_number_from_charpotro,
+            LA_name,
+            LV_name,
+            MV_name,
+            dest_from,
+            dest_to,
+            capacity_ton,
+            rate,
+            sixty_percent_payment,
+            forty_percent_payment,
+            damarage,
+            second_trip,
+            third_trip,
+            direct_trip,
+        ],
+        (err, result) => {
+            if (err) console.log(err);
+            console.log(result);
+            res.send(result);
+        }
+    );
+}
+function getChq_approval(req, res, db){
+    const sqlSelect = "SELECT * from chq_approval";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    });
+}
+function updateChq_approval(req, res, db){
+    //console.log("update job info in backend");
+    const id = req.body.id;
+    const order_number = req.body.new_order_number;
+    const job_number = req.body.new_job_number;
+    const date_from_charpotro = req.body.new_date_from_charpotro;
+    const cp_number_from_charpotro = req.body.new_cp_number_from_charpotro;
+    const LA_name = req.body.new_LA_name;
+    const LV_name = req.body.new_LV_name;
+    const MV_name = req.body.new_MV_name;
+    const dest_from = req.body.new_dest_from;
+    const dest_to = req.body.new_dest_to;
+    const capacity_ton = req.body.new_capacity_ton;
+    const rate = req.body.new_rate;
+    const sixty_percent_payment = req.body.new_sixty_percent_payment;
+    const forty_percent_payment = req.body.new_forty_percent_payment;
+    const damarage = req.body.new_damarage;
+    const second_trip = req.body.new_second_trip;
+    const third_trip = req.body.new_third_trip;
+    const direct_trip = req.body.new_direct_trip;
+    const sqlUpdate =
+        "UPDATE chq_approval SET order_number=?, job_number=?, date_from_charpotro=?, cp_number_from_charpotro=?, LA_name=?, LV_name=?, MV_name=?, dest_from=?, dest_to=?, capacity_ton=?, rate=?, sixty_percent_payment=?, forty_percent_payment=?, damarage=?, second_trip=?, third_trip=?, direct_trip=? WHERE id=?";
+    db.query(
+        sqlUpdate,
+        [
+            order_number,
+            job_number,
+            date_from_charpotro,
+            cp_number_from_charpotro,
+            LA_name,
+            LV_name,
+            MV_name,
+            dest_from,
+            dest_to,
+            capacity_ton,
+            rate,
+            sixty_percent_payment,
+            forty_percent_payment,
+            damarage,
+            second_trip,
+            third_trip,
+            direct_trip,
+            id,
+        ],
+        (err, result) => {
+            if (err) console.log(err);
+            console.log(result);
+
+            res.send(result);
+        }
+    );
+}
+function deleteChq_approval(req, res, db){
+    //console.log("Delete status in backend");
+    const id = req.body.Chq_id;
+    const sqlDelete = "DELETE from chq_approval where id= ?";
+    db.query(sqlDelete, [id], (err, result) => {
+        if (err) console.log(err);
+        //console.log(result)
+        if (!err) {
+            res.send("success");
+        }
+    });
+}
+
+
+module.exports.addChq_approval = addChq_approval;
+module.exports.getChq_approval = getChq_approval;
+module.exports.updateChq_approval = updateChq_approval;
+module.exports.deleteChq_approval = deleteChq_approval;
--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const {addJob, getJob, updatejob, deleteJob, getComodity, fetchOrderNumber} = re
 const {addCurrentStatus, getCurrentStatus, updateCurrentStatus, deleteCurrentStatus} = require("./apis/current_status");
 const {addDamarage, getDamarage, updateDamarage, deleteDamarage} = require("./apis/damarage_dispatch");
 const {addChq_due, getChq_due, updateChq_due, deleteChq_due} = require("./apis/chq_due_list");
+const {addChq_approval, getChq_approval, updateChq_approval, deleteChq_approval} = require("./apis/chq_approval");
 
 // MySQL
 const mysql = require("mysql");
@@ -159,128 +160,21 @@ app.post("/management/updatechq", (req, res) => {
 //------------------- Chq Approval-----------------------------
 // Get Chq Approval
 app.get("/management/getchqapproval", (req, res) => {
-    const sqlSelect = "SELECT * from chq_approval";
-    db.query(sqlSelect, (err, result) => {
-        res.send(result);
-    });
+    getChq_approval(req, res, db);
 });
 
 //Insert Chq
 app.post("/management/insertchq_approval", (req, res) => {
-    console.log("submit in backend");
-    const order_number = req.body.order_number;
-    const job_number = req.body.job_number;
-    const date_from_charpotro = req.body.date_from_charpotro;
-    const cp_number_from_charpotro = req.body.cp_number_from_charpotro;
-    const LA_name = req.body.LA_name;
-    const LV_name = req.body.LV_name;
-    const MV_name = req.body.MV_name;
-    const dest_from = req.body.dest_from;
-    const dest_to = req.body.dest_to;
-    const capacity_ton = req.body.capacity_ton;
-    const rate = req.body.rate;
-    const sixty_percent_payment = req.body.sixty_percent_payment;
-    const forty_percent_payment = req.body.forty_percent_payment;
-    const damarage = req.body.damarage;
-    const second_trip = req.body.second_trip;
-    const third_trip = req.body.third_trip;
-    const direct_trip = req.body.direct_trip;
-    const create_chq =
-        "INSERT INTO chq_approval (order_number, job_number, date_from_charpotro, cp_number_from_charpotro, LA_name, LV_name, MV_name, dest_from, dest_to, capacity_ton, rate, sixty_percent_payment, forty_percent_payment, damarage, second_trip, third_trip, direct_trip) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-    db.query(
-        create_chq,
-        [
-            order_number,
-            job_number,
-            date_from_charpotro,
-            cp_number_from_charpotro,
-            LA_name,
-            LV_name,
-            MV_name,
-            dest_from,
-            dest_to,
-            capacity_ton,
-            rate,
-            sixty_percent_payment,
-            forty_percent_payment,
-            damarage,
-            second_trip,
-            third_trip,
-            direct_trip,
-        ],
-        (err, result) => {
-            if (err) console.log(err);
-            console.log(result);
-            res.send(result);
-        }
-    );
+    addChq_approval(req, res, db);
 });
 // Delete Chq
 app.post("/management/deletechq_approval", (req, res) => {
-    console.log("Delete status in backend");
-    const id = req.body.Chq_id;
-    const sqlDelete = "DELETE from chq_approval where id= ?";
-    db.query(sqlDelete, [id], (err, result) => {
-        if (err) console.log(err);
-        //console.log(result)
-        if (!err) {
-            res.send("success");
-        }
-    });
+    deleteChq_approval(req, res, db);
 });
 
 // Uopdate Chq
 app.post("/management/updatechq_approval", (req, res) => {
-    console.log("update job info in backend");
-    const id = req.body.id;
-    const order_number = req.body.new_order_number;
-    const job_number = req.body.new_job_number;
-    const date_from_charpotro = req.body.new_date_from_charpotro;
-    const cp_number_from_charpotro = req.body.new_cp_number_from_charpotro;
-    const LA_name = req.body.new_LA_name;
-    const LV_name = req.body.new_LV_name;
-    const MV_name = req.body.new_MV_name;
-    const dest_from = req.body.new_dest_from;
-    const dest_to = req.body.new_dest_to;
-    const capacity_ton = req.body.new_capacity_ton;
-    const rate = req.body.new_rate;
-    const sixty_percent_payment = req.body.new_sixty_percent_payment;
-    const forty_percent_payment = req.body.new_forty_percent_payment;
-    const damarage = req.body.new_damarage;
-    const second_trip = req.body.new_second_trip;
-    const third_trip = req.body.new_third_trip;
-    const direct_trip = req.body.new_direct_trip;
-    const sqlUpdate =
-        "UPDATE chq_approval SET order_number=?, job_number=?, date_from_charpotro=?, cp_number_from_charpotro=?, LA_name=?, LV_name=?, MV_name=?, dest_from=?, dest_to=?, capacity_ton=?, rate=?, sixty_percent_payment=?, forty_percent_payment=?, damarage=?, second_trip=?, third_trip=?, direct_trip=? WHERE id=?";
-    db.query(
-        sqlUpdate,
-        [
-            order_number,
-            job_number,
-            date_from_charpotro,
-            cp_number_from_charpotro,
-            LA_name,
-            LV_name,
-            MV_name,
-            dest_from,
-            dest_to,
-            capacity_ton,
-            rate,
-            sixty_percent_payment,
-            forty_percent_payment,
-            damarage,
-            second_trip,
-            third_trip,
-            direct_trip,
-            id,
-        ],
-        (err, result) => {
-            if (err) console.log(err);
-            console.log(result);
-
-            res.send(result);
-        }
-    );
+    updateChq_approval(req, res, db);
 });
 
 //------------------- Payment -----------------------------
